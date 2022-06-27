aae01649bfb87b0a972f1660de16cf94109f1829
chenzhenfang <chenzhenfang@flashexpress.com>
2022-06-27 11:23:20 +0800

    feat: fix bug

--- deploy.sh
+++ deploy.sh
@@ -50,6 +50,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:code1992-clown/web-blog.git main:gh-pages
+git push -f git@github.com:code1992-clown/web-blog.git master:gh-pages
 
 cd -
